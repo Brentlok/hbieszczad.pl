4ed4c8bf891bc073fd9deec8463a264ad99e61d7
Hubert Bieszczad <brentlokk@gmail.com>
2022-11-26 16:19:43 +0100

    Add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 export default defineConfig({
   output: 'server',
   site: 'https://example.com',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind()],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
